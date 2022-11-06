27ce282fad1d520a9f50b7465b8f95f1ad4483f5
Bret Barkley <brb171@pitt.edu>
2022-11-06 01:10:18 -0400

    .\sources\srd.py

--- sources/parsed/generated_spells.py
+++ sources/parsed/generated_spells.py
@@ -10171,7 +10171,7 @@ class TransportViaPlants(spells.Spell):
                          duration="1 round",
                          description="This spell creates a magical link between a Large or larger " +
                                     "inanimate plant within range and another plant, at any " +
-                                    "distance, on the same plane of existence. You must have seen " +
+                                        "distance, on the same plane of existence. You must have seen " +
                                     "or touched the destination plant at least once before. For the " +
                                     "duration, any creature can step into the target plant and exit " +
                                     "from the destination plant by using 5 feet of movement. ",
